5baaf8dac6284863f0c877dfaf0815e965737dc1
cenmcc <15565555045@163.com>
2023-05-18 11:18:57 +0800

    修改配置

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+console.log('11111111111')
 </script>
 
 <template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,75 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from "vite";
+import type { ConfigEnv, UserConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path, { resolve } from "path";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+/** @type {import('vite').UserConfig} */
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
+  const viteEnv = loadEnv(mode, process.cwd());
+  return {
+    base: viteEnv.VITE_PUBLIC_PATH,
+    resolve: {
+      alias: {
+        /** @ 符号指向 src 目录 */
+        "@": resolve(__dirname, "./src"),
+      },
+    },
+    server: {
+      /** 是否开启 HTTPS */
+      https: false,
+      /** 设置 host: true 才可以使用 Network 的形式，以 IP 访问项目 */
+      host: true, // host: "0.0.0.0"
+      /** 端口号 */
+      port: 3333,
+      /** 是否自动打开浏览器 */
+      open: false,
+      /** 跨域设置允许 */
+      cors: true,
+      /** 端口被占用时，是否直接退出 */
+      strictPort: false,
+      /** 接口代理 */
+      proxy: {
+        "/apis": {
+          target:
+            "https://mock.mengxuegu.com/mock/63218b5fb4c53348ed2bc212/api/v1",
+          ws: true,
+          /** 是否允许跨域 */
+          changeOrigin: true,
+          rewrite: (path: string): string => path.replace("/apis", ""),
+        },
+      },
+    },
+    build: {
+      /** 消除打包大小超过 500kb 警告 */
+      chunkSizeWarningLimit: 2000,
+      /** Vite 2.6.x 以上需要配置 minify: "terser", terserOptions 才能生效 */
+      minify: "terser",
+      /** 在打包代码时移除 console.log、debugger 和 注释 */
+      terserOptions: {
+        compress: {
+          drop_console: false,
+          drop_debugger: true,
+          pure_funcs: ["console.log"],
+        },
+        format: {
+          /** 删除注释 */
+          comments: false,
+        },
+      },
+      /** 打包后静态资源目录 */
+    },
+    plugins: [
+      vue(),
+      AutoImport({
+        // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+        imports: ["vue"],
+      }),
+      Components({
+        // 指定自动导入的组件位置，默认是 src/components
+        // dirs: ['src/components'],
+      }),
+    ],
+  };
+});
